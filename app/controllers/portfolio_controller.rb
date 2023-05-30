d3eec76f594b57313da79ceed8b548b80da0127b
Alice Palmer Brown <palmerbrownalice@gmail.com>
2023-05-30 17:05:09 +0100

    early stages

---
+++ app/controllers/portfolio_controller.rb
@@ -0,0 +1,13 @@
+class PortfolioController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def cv
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get '/', to: 'portfolio#index'
+  get '/about', to: 'portfolio#about', as: 'about'
+  get '/cv', to: 'portfolio#cv'
   # Defines the root path route ("/")
   # root "articles#index"
 end
